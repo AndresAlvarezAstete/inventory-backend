58b13ca351fb3009d70aadd0a00052f05ec5a46a
Andres Alvarez <andres.alvarez.as@gmail.com>
2023-02-14 18:29:41 -0300

    Eliminar categoria

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,4 +74,16 @@ public class CategoryRestController {
 		return response;
 	}
 	
+	/**
+	 * delete categories
+	 * @param id
+	 * @return
+	 */
+	
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> deleteById(@PathVariable Long id) {
+		
+		ResponseEntity<CategoryResponseRest> response = service.deleteById(id);
+		return response;
+	}
 }
